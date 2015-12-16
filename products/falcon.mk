c51a114596b8b53785c432fd18540a14d7bbe379
C-Aniruddh <chandratre.aniruddh@gmail.com>
2015-12-16 10:15:08 -0500

    vendor: remove products

--- products/falcon.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
-
-# Inherit some common flex stuff.
-$(call inherit-product, vendor/flex/config/common_full_phone.mk)
-
-PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := flex_falcon
