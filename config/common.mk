ResurrectionRemix/android_vendor_resurrection

vendor: add SuperSU 2.52
Signed-off-by: Akhil Narang <something@akhilnarang.xyz>

config/common.mk
@@ -227,6 +227,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 CM_DISPLAY_VERSION := $(CM_VERSION)
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/cm/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 

srisurya95/vendor_flex

config/common.mk
@@ -230,6 +230,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 FLEX_DISPLAY_VERSION := $(FLEX_VERSION)
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/flex/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/flex/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 