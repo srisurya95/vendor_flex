a77f2c0684852af6f9454cf1c7890b1e5b56078d
Alectradar <gokulbalram@gmail.com>
2015-12-26 15:19:32 +0530

    i9300: Correct makefile

--- products/i9300.mk
+++ products/i9300.mk
@@ -13,31 +13,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Common flex stuff
+# Release name
+PRODUCT_RELEASE_NAME := i9300
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/flex/config/common_full_phone.mk)
 
-# This is where we'd set a backup provider if we had one
-#$(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, device/samsung/i9300/i9300.mk)
+# Inherit device configuration
+$(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/flex/config/nfc_enhanced.mk)
-
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := flex_i9300
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
+PRODUCT_NAME := flex_i9300
 PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9300
+PRODUCT_MANUFACTURER := samsung
 
-# Override common i9300 properties to show properly i.e. in google services
-# This is required to solve some apps being incompatible with our device
-# Values should be equal to newest Sammy's stock release, or adapted to Omni
-# Currently they're stock, but we may want to change them in the future, such as:
-# BUILD_FINGERPRINT="samsung/m0xx/m0:4.4.2/KVT49L/I9300OMNI:user/release-keys"
-# PRIVATE_BUILD_DESC="m0xx-user 4.4.2 KVT49L I9300OMNI release-keys"
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGNA7:user/release-keys" \
-    PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGNA7 release-keys" \
-    TARGET_DEVICE=m0 \
-    PRODUCT_NAME=m0xx
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
