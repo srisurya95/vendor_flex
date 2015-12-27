0c0fbb533d8e4f5860d31fd16031a6bfa08f528d
C-Aniruddh <chandratre.aniruddh@gmail.com>
2015-12-27 03:59:16 -0500

    vendor: introduce xperia m (nicki)

---
+++ products/nicki.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Enhanced NFC
+$(call inherit-product, vendor/flex/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/flex/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/sony/nicki/nicki.mk)
+
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := nicki
+PRODUCT_NAME := flex_nicki
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := nicki
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_CHARACTERISTICS := phone
+
+# Release name
+PRODUCT_RELEASE_NAME := Xperiam
