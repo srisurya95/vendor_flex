7019d0c784f14cdfd94d5c87447bf26284ab128c
Akhil Narang <akhilnarang.1999@gmail.com>
2015-12-27 09:06:25 -0500

    flexify
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ tools/flex.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+./vendor/flex/tools/colors
+
+function e()
+{
+echo -e $bldred "$@" $txtrst;
+}
+
+f=$(which figlet);
+if [ "$f" == "" ];
+then
+e "";
+e "  ______ _            ____   _____";
+e " |  ____| |          / __ \ / ____|";
+e " | |__  | | _____  _| |  | | (___";
+e " |  __| | |/ _ \ \/ / |  | |\___ \";
+e " | |    | |  __/>  <| |__| |____) |";
+e " |_|    |_|\___/_/\_\\____/|_____/";
+e "";
+
+e "Please install 'figlet' tool";
+else
+figlet "FlexOS";
+fi
